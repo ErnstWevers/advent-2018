b10309cd39025bf2918d7f797a9be62a5968e4d7
ErnstWevers <ernstwevers@gmail.com>
2018-12-07 22:48:21 +0100

    day two

---
+++ 02/part_1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const stream = require('stream');
+
+const readStream = fs.createReadStream('./input/input.txt');
+const handleStream = new stream.Transform();
+const getFreq = new stream.Transform();
+let twos = 0;
+let threes = 0;
+
+handleStream._transform = (chunk, encoding, done) => {
+  const data = chunk.toString();
+  const lines = data.split('\n');
+  lines.forEach(line => handleStream.push(line));
+  done();
+};
+
+getFreq._transform = (chunk, encoding, done) => {
+  const boxId = chunk.toString().split('');
+  const freqs = boxId.map(e =>
+    boxId.reduce((a, c) => (e === c ? a + 1 : a), 0)
+  );
+  if (freqs.includes(2)) twos++;
+  if (freqs.includes(3)) threes++;
+  done();
+};
+
+readStream
+  .pipe(handleStream)
+  .pipe(getFreq)
+  .on('finish', () => console.log(`This is the checksum: ${twos * threes}`));
---
+++ 02/part_2.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const stream = require('stream');
+
+const readStream = fs.createReadStream('./input/input.txt');
+const handleStream = new stream.Transform();
+const boxList = [];
+
+handleStream._transform = (chunk, encoding, done) => {
+  const data = chunk.toString();
+  const lines = data.split('\n');
+  lines.forEach(line => {
+    if (line !== '') boxList.push(line);
+  });
+  done();
+};
+
+const compareBoxes = (foo, bar) => {
+  let diff = 0;
+  for (let i = 0; i < foo.length; i++) {
+    if (foo[i] !== bar[i]) diff++;
+  }
+  return diff < 2;
+};
+
+const subtractBoxes = (foo, bar) => {
+  const common = [];
+  for (let i = 0; i < foo.length; i++) {
+    if (foo[i] === bar[i]) common.push(foo[i]);
+  }
+  return common.join('');
+};
+
+readStream.pipe(handleStream).on('finish', () => {
+  for (let i = 0; i < boxList.length - 1; i++) {
+    for (let j = i + 1; j < boxList.length; j++) {
+      if (compareBoxes(boxList[i], boxList[j])) {
+        console.log(
+          `the common letters is the IDs are: ${subtractBoxes(
+            boxList[i],
+            boxList[j]
+          )}`
+        );
+        return null;
+      }
+    }
+  }
+});
